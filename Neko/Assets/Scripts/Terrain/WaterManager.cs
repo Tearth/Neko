e257e833a23d5536008411781a9d54edf3dcd4e9
tearth <Tearth@users.noreply.github.com>
2018-08-13 14:22:03 +0200

    Add water to scene.

---
+++ Neko/Assets/Scripts/Terrain/WaterManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class WaterManager : MonoBehaviourSingleton<WaterManager>
+{
+    public float WaterLevel;
+
+    private TerrainManager _terrainManager;
+
+    private void Start()
+    {
+        _terrainManager = TerrainManager.Instance;
+
+        var terrainWidth = (float)_terrainManager.ChunksCount.x * _terrainManager.ChunkSize;
+        var terrainHeight = (float)_terrainManager.ChunksCount.y * _terrainManager.ChunkSize;
+
+        transform.position = new Vector3(terrainWidth / 2, WaterLevel, terrainHeight / 2);
+        transform.localScale = new Vector3(terrainWidth, 1, terrainHeight);
+    }
+
+    private void Update()
+    {
+
+    }
+}
