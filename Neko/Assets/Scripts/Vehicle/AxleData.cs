a53c4a3e1cf384c5dde27abb5e8eb23f1cef3e41
tearth <Tearth@users.noreply.github.com>
2018-08-21 20:18:06 +0200

    Add test vehicle.

---
+++ Neko/Assets/Scripts/Vehicle/AxleData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class AxleData
+{
+    public GameObject LeftWheel;
+    public WheelCollider LeftWheelCollider;
+
+    public GameObject RightWheel;
+    public WheelCollider RightWheelCollider;
+
+    public bool Motor;
+    public bool Steering;
+}
---
+++ Neko/Assets/Scripts/Vehicle/VehicleEntity.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleEntity : MonoBehaviour
+{
+    public List<AxleData> Axles;
+
+    void Start()
+    {
+        foreach (AxleData axle in Axles)
+        {
+            axle.LeftWheelCollider.ConfigureVehicleSubsteps(5, 10, 12);
+            axle.RightWheelCollider.ConfigureVehicleSubsteps(5, 10, 12);
+        }
+    }
+
+    void Update()
+    {
+        var motor = 0;
+        var steering = 0;
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            motor = 400;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            motor = -400;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            steering = 45;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            steering = -45;
+        }
+
+        foreach (AxleData axle in Axles)
+        {
+            if (axle.Steering)
+            {
+                axle.LeftWheelCollider.steerAngle = steering;
+                axle.RightWheelCollider.steerAngle = steering;
+            }
+            if (axle.Motor)
+            {
+                axle.LeftWheelCollider.motorTorque = motor;
+                axle.RightWheelCollider.motorTorque = motor;
+            }
+
+            ApplyColliderParametersToWheel(axle.LeftWheel, axle.LeftWheelCollider);
+            ApplyColliderParametersToWheel(axle.RightWheel, axle.RightWheelCollider);
+        }
+    }
+
+    private void ApplyColliderParametersToWheel(GameObject wheel, WheelCollider collider)
+    {
+        Vector3 position;
+        Quaternion quaternion;
+
+        collider.GetWorldPose(out position, out quaternion);
+
+        wheel.transform.position = position;
+        wheel.transform.eulerAngles = quaternion.eulerAngles + new Vector3(90, 0, 90);
+    }
+}
